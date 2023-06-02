783e3ed0865e007af0df2f53e182adc3e19ba03e
Akelarre Taltos <93266473+Taltos87@users.noreply.github.com>
2023-06-02 18:58:33 +0100

    Create Input Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-<div className="Form">
-    <h1>Weather Card</h1> 
+const Form = () => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="input-group mb-3">
+                <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Enter City" aria-label="Enter City" aria-describedby="button-addon2" 
+                onChange={handleImputChange}/>
     </div>
+    <button className="btn btn-primery" type="submit"> Get Weather</button>
+    </form>);
+};
