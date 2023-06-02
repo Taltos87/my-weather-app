623edbf24b6be9dd1359e9d63e2f5889b44d16d6
Akelarre Taltos <93266473+Taltos87@users.noreply.github.com>
2023-06-02 23:02:24 +0100

    define city and mor eerror

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,11 @@
+import React, { useState } from 'react';
 import { Button } from 'bootstrap';
-import React from 'react';
 
 const Form = (props) => {
+    const { city, setCity } = useState('');
+    const handleInputChange  = (event) => {
+        setCity(event.target.value);}
+    
     return (
         <div className="Form">
          <div className="input-group mb-3">
