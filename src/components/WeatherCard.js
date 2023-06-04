1c4ca25127a79d55e3356759c5f50155a8563360
Akelarre Taltos <93266473+Taltos87@users.noreply.github.com>
2023-06-04 12:16:21 +0100

    removed card data

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -7,18 +7,14 @@ const WeatherCard = ({weatherData}) => {
     <div className='card'>
         <div cardNmae='card-body'>
         <h1 className='card-title'>{weatherData.name}</h1>
+        <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt='weather icon' />
         <p> Tempreture: {Math.round(weatherData.main.temp)}°C</p>
         <p> Description: {weatherData.weather[0].description}</p>
-        <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt='weather icon' />
-        {/* <p> Humidity: {weatherData} %</p> */}
         <p> Wind Speed: {weatherData.wind.speed} km/h</p>
         <p> Humidity: {weatherData.main.humidity} %</p>
         <p> Clouds: {weatherData.clouds.all} %</p>
-        <p> Date: {new Date(weatherData.dt * 1000).toLocaleDateString()}</p>
-        <p> Time: {new Date(weatherData.dt * 1000).toLocaleTimeString()}</p>
+      
         <p> Feels Like: {Math.round(weatherData.main.feels_like)}°C</p>
-        <p> Min Temp: {Math.round(weatherData.main.temp_min)}°C</p>
-        <p> Max Temp: {Math.round(weatherData.main.temp_max)}°C</p>
         <p> Pressure: {weatherData.main.pressure} hPa</p>
         <p> Visibility: {weatherData.visibility} m</p>
         <p> Geo Coordinates: {weatherData.coord.lat}°N, {weatherData.coord.lon}°E</p>
